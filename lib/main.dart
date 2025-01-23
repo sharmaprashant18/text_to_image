c6940f49378a048c4fd8b54e56c483b69df8f413
sharmaprashant18 <ps3348644@gmail.com>
2025-01-23 21:27:24 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:texttoimage/view/homepage.dart';
 
 void main() {
@@ -10,6 +11,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,8 +11,19 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   @override
+  final ImagePicker imagePicker = ImagePicker();
+  final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+  Future<void> pickImage(ImageSource source) async {
+    try {} catch (e) {
+      throw Exception('Error picking image:$e');
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Image to Text'),
+      ),
       body: ListView(
         children: [
           Center(
