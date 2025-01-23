42c9305779bca92d9debdf14f9a41fa720d3dbe3
sharmaprashant18 <ps3348644@gmail.com>
2025-01-23 20:57:02 +0545

    Changed

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
