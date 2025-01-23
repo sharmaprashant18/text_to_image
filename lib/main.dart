94aa09590abee317a6163fe3fff50f466a00353f
sharmaprashant18 <ps3348644@gmail.com>
2025-01-23 20:50:33 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-import'
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
